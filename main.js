369ac780cc5a5722cc83f084f16035c8d24652bc
elive7 <parksub020704@gmail.com>
2023-05-05 18:48:49 +0900

    layout.ejs
    layout.ejs 성공!!!

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const port = 3000,
     express = require("express"),
-    app = express();
-//const { logger } = require("./config/winston");
+    app = express(),
+    layouts = require("express-ejs-layouts");
+    //const { logger } = require("./config/winston");
 
-app.use("/", (req,res) =>
-    {res.send("안녕");}
- )
-
+ app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(layouts);
+app.get(
+    "/", (req,res) =>
+    {res.render("index");}
+)
 app.listen(port,() => {
     console.log("서버 실행 중");
 }
