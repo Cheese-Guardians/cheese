537d9a4faaf5d5737dbb79d6e36ebe17482ae119
handagyeong <80438961+handagyeong@users.noreply.github.com>
2023-05-12 18:43:03 +0900

    fileupload

--- main.js
+++ main.js
@@ -61,8 +61,15 @@ const storage = multer.diskStorage({
   // 파일 업로드를 처리하는 라우트 핸들러
   app.post('/upload', upload.single('file'), (req, res) => {
     if (!req.file) {
-      res.status(400).send('파일이 없습니다.');
+        // 파일이 없을 경우 팝업 창 표시 후 확인 시 "/" 페이지로 리다이렉션
+        return res.send(`
+          <script>
+            if (confirm('파일이 없습니다. 확인을 누르면 메인 페이지로 돌아갑니다.')) {
+              window.location.href = "/";
+            }
+          </script>
+        `);
     } else {
-        res.render("/")
+        res.render("calendar");
     }
   });
