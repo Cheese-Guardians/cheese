cb315e740fc3a8e11734ea026eb5d2cc5919ccd8
이하은 <haeun161@naver.com>
2023-05-12 16:19:35 +0900

    connect DB & calendar.ejs update

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-const port = 3003,
+const db = mysql.createPool({ 
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PW, 
+    port: process.env.DB_PORT, 
+    database: process.env.DB_NAME, 
+    waitForConnections: true, 
+    insecureAuth: true
+});
+
+let sql = 'SELECT * FROM category';
+let[rows,fields] = await db.query(sql);
+console.log(rows);
+
+const port = 3000,
     express = require("express"),
     app = express(),
     layouts = require("express-ejs-layouts");
     //const { logger } = require("./config/winston");
-var multer=require('multer');
-var upload=multer({dest:'uploads/'});
 
 
- app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(layouts);
 app.get(
     "/calendar", (req,res) =>
-    {res.render("calendar");}
+    {res.render("\calendar/calendar.ejs");}
 )
 app.get(
     "/login", (req,res) =>
@@ -22,17 +34,43 @@ app.get(
     "/signup", (req,res) =>
     {res.render("signup");}
 )
+
+app.get(
+    "/community", (req,res) =>
+    {res.render("\community/community.ejs");}
+)
+
 app.get(
     "/", (req,res) =>
     {res.render("calendar");}
 )
+
+
 
 app.listen(port,() => {
     console.log("서버 실행 중");
 }
 )
 
-app.post('/upload', upload.single('memorizes'), function(req,res){
-    res.send('Uploaded! : '+req.file); // object를 리턴함
-    console.log(req.file); 
-})
+require('dotenv').config({path: "database.env"});
+const mysql = require('mysql2/promise'); 
+/*
+let test = async () => {
+    const db = mysql.createPool({ 
+        host: process.env.DB_HOST, 
+        user: process.env.DB_USER, 
+        password: process.env.DB_PW, 
+        port: process.env.DB_PORT, 
+        database: process.env.DB_NAME, 
+        waitForConnections: true, 
+        insecureAuth: true
+    });
+
+    let sql = 'SELECT * FROM category';
+    let[rows,fields] = await db.query(sql);
+    console.log(rows);
+};
+
+
+test()
+*/
