2b4399f35981594fe6e64d82a061180d9381a007
이하은 <haeun161@naver.com>
2023-05-12 12:06:58 +0900

    calendar fixed
    캘린더 화면 수정 중.
    캘린더 옆 증상 체크 부분 찾는중

--- main.js
+++ main.js
@@ -25,10 +25,18 @@ app.get(
     "/signup", (req,res) =>
     {res.render("signup");}
 )
+
+app.get(
+    "/community", (req,res) =>
+    {res.render("\community/community.ejs");}
+)
+
 app.get(
     "/", (req,res) =>
     {res.render("calendar");}
 )
+
+
 
 app.listen(port,() => {
     const dir = "./uploads";
