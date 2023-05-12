033a1da8daa443dd6434bd0f6b0eedfef829032e
이하은 <haeun161@naver.com>
2023-05-12 20:19:46 +0900

    작업들 합치는 중
    다경 + 수빈 + 하은

--- main.js
+++ main.js
@@ -17,12 +17,18 @@ const pool = mysql.createPool({
     //onsole.log(rows);
 const port = 3000,
     express = require("express"),
+    cors=require("cors")
     app = express(),
-    layouts = require("express-ejs-layouts");
+    fs = require("fs"),
+    layouts = require("express-ejs-layouts")
     //const { logger } = require("./config/winston");
+    multer  = require('multer')
+    path = require('path');
 
 
- app.set("view engine", "ejs");
+
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 app.use(layouts);
 app.get(
@@ -41,11 +47,11 @@ app.get(
 app.get(
     "/login", (req,res) =>
     {res.render("login");}
-)
+);
 app.get(
     "/signup", (req,res) =>
     {res.render("signup");}
-)
+);
 app.get(
     "/", (req,res) =>
     {
@@ -60,7 +66,50 @@ app.get(
 );
 
 app.listen(port,() => {
-    console.log("서버 실행 중");
-}
-)
+  const dir = "./uploads";
+  if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+  }
+  console.log("서버 실행 중");
+  }
+);
 
+
+
+
+// Multer 설정
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // 업로드된 파일을 저장할 폴더 경로
+  },
+  filename: (req, file, cb) => {
+    const fileName = file.fieldname + '-' + Date.now() + path.extname(file.originalname); // 저장될 파일 이름 설정
+    cb(null, fileName);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+
+
+// 파일 업로드를 처리하는 라우트 핸들러
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+      // 파일이 없을 경우 팝업 창 표시 후 확인 시 "/" 페이지로 리다이렉션
+      return res.send(`
+        <script>
+          if (confirm('파일이 없습니다. 확인을 누르면 메인 페이지로 돌아갑니다.')) {
+            window.location.href = "/";
+          }
+        </script>
+      `);
+  } else {
+    const sql = "select * from category;";
+    pool.query(sql, (err, results) => {
+        if (err) {
+          throw err;
+        }
+        res.render('\calendar/calendar.ejs', { data: results });
+    });
+  }
+});
--- public/js/main.js
+++ public/js/main.js
@@ -447,3 +447,141 @@ function autoLeftPad(num, digit) {
   }
   return num;
 }
+
+// 슬라이드 이미지 선택자
+const sliderImages = document.querySelectorAll('.slide')
+// 오른쪽 화살표 선택자
+const arrowRight = document.querySelector('.arrow--right')
+// 왼쪽 화살표 선택자
+const arrowLeft = document.querySelector('.arrow--left')
+// 현재 보여지는 슬라이드 번호
+let current = 0;
+
+// 동그라미들 선택자
+const dots = document.querySelectorAll('.dot')
+const dot1 = document.querySelector('.dot1')
+const dot2 = document.querySelector('.dot2')
+const dot3 = document.querySelector('.dot3')
+const dot4 = document.querySelector('.dot4')
+const dot5 = document.querySelector('.dot5')
+const dot6 = document.querySelector('.dot6')
+
+// 슬라이드 이미지 리셋
+const reset = () => {
+    // 슬라이드 이미지 모두 보이지 않는 상태로 설정
+    sliderImages.forEach((el) => el.style.display = 'none')
+    dots.forEach((el) => el.style.background = '#F6F5F0')
+}
+
+// 자동 슬라이드 기능을 위한 함수
+const autoSlide = () => {
+    // 모든 슬라이드 이미지를 보이지 않는 상태로 설정
+    reset()
+    // 마지막 슬라이드 이미지라면,
+    if (current === sliderImages.length - 1) {
+        // current 값을 -1로 설정
+        current = -1
+    } 
+    
+    // 마지막 슬라이드 이미지가 아니라면,
+    // current 값에 1을 더함
+    current++
+    // 슬라이드 이미지 배열에서 index값이 current인 이미지를 보이는 상태로 설정 
+    sliderImages[current].style.display = 'block'
+    dots[current].style.background = '#1107ff'
+}
+
+// 수동 슬라이드를 위한 함수들
+// 1. 왼쪽 화살표에 클릭 이벤트가 발생할 시 작동할 함수
+const slideLeft = () => {
+    // 모든 슬라이드 이미지 display를 none으로 리셋
+    reset()
+    // current(현재 보여지는 슬라이드 이미지)의 
+    // 이전 슬라이드 이미지가 보이도록 스타일 변경
+    sliderImages[current - 1].style.display = 'block';
+    dots[current - 1].style.background = '#1107ff'
+    // current값을 1만큼 뺌
+    current--;
+}
+
+// 2. 오른쪽 화살표에 클릭 이벤트가 발생할 시 작동할 함수
+const slideRight = () => {
+    // 모든 슬라이드 이미지 display를 none으로 리셋
+    reset()
+    // current(현재 보여지는 슬라이드 이미지)의 
+    // 다음 슬라이드 이미지가 보이도록 스타일 변경 
+    sliderImages[current + 1].style.display = 'block';
+    dots[current + 1].style.background = '#1107ff'
+    // current값을 1만큼 더함
+    current++;
+}
+
+// 오른쪽 화살표를 클릭할 시 작동하는 익명의 함수
+arrowLeft.addEventListener('click', function() {
+    // 만약 current값이 0이라면,
+    // 즉 현재 보이는 슬라이드 이미지가 첫번째 순서라면,
+    if(current === 0) {
+        // current값을 슬라이드 이미지 배열의 길이로 변경
+        current = sliderImages.length;
+    }
+    slideLeft()
+})
+
+// 왼쪽 화살표를 클릭할 시 작동하는 익명의 함수
+arrowRight.addEventListener('click', function() {
+    // 만약 current값이 슬라이드 이미지 배열 길이 - 1만큼의 값이라면,
+    // 즉 현재 보이는 슬라이드 이미지가 마지막 순서라면,
+    if(current === sliderImages.length - 1) {
+        // current값을 -1로 설정
+        current = -1;
+    }
+    slideRight()
+})
+
+// 첫번째 동그라미 클릭할 시 작동하는 익명의 함수
+dot1.addEventListener("click", function(){
+    reset()
+    current = 0
+    sliderImages[current].style.display = 'block';
+    dots[current].style.background = '#1107ff'
+})
+
+// 두번째 동그라미 클릭할 시 작동하는 익명의 함수
+dot2.addEventListener("click", function(){
+    reset()
+    current = 1
+    sliderImages[current].style.display = 'block';
+    dots[current].style.background = '#1107ff'
+})
+
+// 세번째 동그라미 클릭할 시 작동하는 익명의 함수
+dot3.addEventListener("click", function(){
+    reset()
+    current = 2
+    sliderImages[current].style.display = 'block';
+    dots[current].style.background = '#1107ff'
+})
+
+// 네번째 동그라미 클릭할 시 작동하는 익명의 함수
+dot4.addEventListener("click", function(){
+    reset()
+    current = 3
+    sliderImages[current].style.display = 'block';
+    dots[current].style.background = '#1107ff'
+})
+
+// 다섯번째 동그라미 클릭할 시 작동하는 익명의 함수
+dot5.addEventListener("click", function(){
+    reset()
+    current = 4
+    sliderImages[current].style.display = 'block';
+    dots[current].style.background = '#1107ff'
+})
+
+// 여섯번째 동그라미 클릭할 시 작동하는 익명의 함수
+dot6.addEventListener("click", function(){
+    reset()
+    current = 5
+    sliderImages[current].style.display = 'block';
+    dots[current].style.background = '#1107ff'
+})
