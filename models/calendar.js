d319164acc548daa115eae72d26aa675788c74fa
theunghee02 <20211150@sungshin.ac.kr>
2023-05-16 03:14:46 +0900

    getCalendar api - file(backend)
    frontend 계속 되는 실패
    calendarResult가 정의되지 않았다는 문제 발생
    calendarController.js 파일의 res.render() 문제인 듯싶음

--- models/calendar.js
+++ models/calendar.js
@@ -1,5 +1,14 @@
-//const pool = require('../main');
-//const mysql = require ('mysql2');
+async function selectCalendar(pool, userId) {
+    const selectCalendarQuery = `
+                SELECT server_name, extension 
+                FROM file_memories 
+                WHERE calendar_id = ? and user_id = 'handakyeng';
+                 `;
+  const [userRow] = await pool.promise().query(selectCalendarQuery, userId);
+  return userRow;
+}
+
+// 파일 업로드
 async function insertFileMem(pool, insertFileMemParams) {
     //console.log(typeof(insertFileMemParams[0]));
     //const server_name = parseInt(insertFileMemParams[0]);
@@ -23,5 +32,6 @@ async function insertFileMem(pool, insertFileMemParams) {
 }
 
 module.exports = {
+    selectCalendar,
     insertFileMem,
 }
