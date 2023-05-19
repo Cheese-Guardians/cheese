08220f757d5e602c2a5313f98680f2ae7bb49e19
haeun161 <80445078+haeun161@users.noreply.github.com>
2023-05-19 10:53:52 +0900

    Merge pull request #35 from Cheese-Guardians/theunghee
    merge theunghee to haeun

--- services/calendar.js
+++ services/calendar.js
@@ -2,22 +2,20 @@ const { response } = require('express');
 const pool = require('../main');
 const calendarModel = require('../models/calendar');
 
+exports.retrieveCalendar = async function (userId) {
+    const calendarResult = await calendarModel.selectCalendar(pool, userId);
+    return calendarResult;
+}
+
 exports.createFileMem = async function (server_name, user_name, extension) {
     try {
         const insertFileMemParams = [server_name, user_name, extension];
-        //console.log(typeof(user_name));
-        //console.log(typeof(extension));
-        //const connection = await pool.getConnection(async (conn) => conn); // 데이터베이스 연결
         
         await calendarModel.insertFileMem(pool, insertFileMemParams);
-        
-        //connection.release();
 
         return '성공';
     } catch (err) {
         return 'error';
     }
 }
-
-// req로 받아오는 것 : server_name, user_name, extension
 
