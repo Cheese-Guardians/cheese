f2f4b21fecb328da76d5b70862e78dc7fb4ced7f
Minju Park <80474535+parkminju20211126@users.noreply.github.com>
2023-05-21 03:04:17 +0900

    createuser
    createuser

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
-
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
+//1. 유저 생성 (회원가입)
+router.post('/signup'. usersController.postUsers);
 module.exports = router;
