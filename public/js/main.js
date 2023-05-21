b8bb16d636b6540ed27df39ecb9b6c75d41f7621
elive7 <parksub020704@gmail.com>
2023-05-21 16:39:27 +0900

    calendar test

--- public/js/main.js
+++ public/js/main.js
@@ -585,3 +585,87 @@ dot6.addEventListener("click", function(){
     sliderImages[current].style.display = 'block';
     dots[current].style.background = '#1107ff'
 })
+
+// 아이템들을 담을 아이템 리스트
+let itemList = [];
+// 아이템 중복확인 위한 변수
+let dupl;
+// 추가 버튼에 대한 이벤트
+let addBtn = document.querySelector("#add");
+addBtn.addEventListener("click", addList);
+// 전체삭제 버튼에 대한 이벤트
+let removeAllBtn = document.querySelector("#remove_all");
+removeAllBtn.addEventListener("click", removeList);
+
+// 엔터 입력시 이벤트 발생
+document
+  .querySelector("#item")
+  .addEventListener("keypress", function (e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      addList();
+    }
+  });
+
+// 아이템 추가 메서드
+function addList() {
+  // 아이템 값 담음
+  let item = document.querySelector("#item").value;
+  // console.log(item);
+
+  // 중복체크 실행
+  checkDupl(item);
+
+  // 조건을 확인하고 아이템 리스트에 푸시
+  if (item != "" && dupl) {
+    itemList.push(item);
+    // console.log(itemList);
+
+    // 아이템 input 창 초기화
+    document.querySelector("#item").value = "";
+    document.querySelector("#item").focus();
+
+    // 아이템 리스트 출력
+    showList();
+  }
+}
+
+// 중복확인 메서드
+function checkDupl(item) {
+  // 입력한 아이템이 아이템 리스트에 있는지 확인
+  if (itemList.includes(item)) {
+    alert("이미 추가한 항목입니다.");
+    document.querySelector("#item").value = "";
+    document.querySelector("#item").focus();
+    dupl = false;
+  } else {
+    dupl = true;
+  }
+}
+
+// 아이템리스트 출력 메서드 (테이블)
+// 아이템리스트 출력 메서드 (테이블)
+function showList() {
+  // 아이템 리스트를 for 문을 돌면서 테이블 태그로 생성
+  let list = "<table>";
+  for (let i = 0; i < itemList.length; i++) {
+    list += <tr>
+      <td class="item">
+        <div class="checkbox">
+          <input type="checkbox" id="${i}" />
+        </div>
+        <div class="content">${itemList[i]}</div>
+      </td>
+      </tr>;
+  }
+  list += "</table>";
+
+  // 테이블 태그 출력
+  document.querySelector("#item_list").innerHTML = list;
+
+  // 아이템 리스트에서 체크박스 이벤트를 할당
+  let checkboxes = document.querySelectorAll(".item input[type='checkbox']");
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].onclick = toggleItem;
+  }
+}
