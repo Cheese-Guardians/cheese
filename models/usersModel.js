dc825b7c1562b46d3a47c0c81fac46894f01f924
theunghee02 <20211150@sungshin.ac.kr>
2023-05-22 00:13:19 +0900

    login basic setting

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,3 +1,4 @@
+// 회원가입
 async function insertUserInfo(pool, insertUserPhoneParams, insertUserInfoParams, insertPatientParams) {
   const insertUserPhoneQuery = `
       insert into user_phone (gd_phone, user_name)
@@ -29,6 +30,34 @@ async function insertUserInfo(pool, insertUserPhoneParams, insertUserInfoParams,
   }
 }
 
+// 아이디 확인
+async function selectUserId(pool, user_id) {
+  const selectUserIdQuery = `
+                SELECT email 
+                FROM UserInfo 
+                WHERE email = ?;
+                `;
+  const [emailRows] = await pool.query(selectUserIdQuery, user_id);
+  return emailRows;
+}
+
+// 비밀번호 확인
+async function selectUserPassword(pool, selectUserPasswordParams) {
+  const selectUserPasswordQuery = `
+        SELECT user_id, password
+        FROM UserInfo 
+        WHERE user_id = ? AND password = ?;`;
+  const selectUserPasswordRow = await pool.query(
+      selectUserPasswordQuery,
+      selectUserPasswordParams
+  );
+
+  return selectUserPasswordRow;
+}
+
 module.exports = {
-  insertUserInfo
+  insertUserInfo,
+  selectUserId,
+  selectUserPassword,
+  
 };
--- services/usersService.js
+++ services/usersService.js
@@ -6,6 +6,7 @@ const crypto = require("crypto");
 const { connect } = require("http2");
 const { userInfo } = require("os");
 
+// 회원가입
 exports.createUser = async function (
     user_id,
     password,
@@ -33,7 +34,7 @@ exports.createUser = async function (
       ];
       const insertUserInfoParams = [
         user_id,
-        password,
+        hashedPassword,
         relationship,
         gd_phone
       ];
@@ -61,3 +62,88 @@ exports.createUser = async function (
         return 'error';
     }
   };
+
+  // 아이디 확인
+  exports.userIdCheck = async function (user_id) {
+    const userIdCheckResult = await usersModel.selectUserId(pool, user_id);
+  
+    return userIdCheckResult;
+  };
+
+  // 비밀번호 확인
+  exports.passwordCheck = async function (selectUserPasswordParams) {
+    const passwordCheckResult = await usersModel.selectUserPassword(
+        pool,
+        selectUserPasswordParams
+    );
+    return passwordCheckResult[0];
+  };
+
+  // // 계정 상태 확인
+  // exports.accountCheck = async function (user_id) {
+  //   const userAccountResult = await usersModel.selectUserAccount(pool, user_id);
+
+  //   return userAccountResult;
+  // };
+
+  // 로그인
+  exports.postSignIn = async function (user_id, password) {
+    try {
+      // 아이디 여부 확인
+      const userIdRows = await userIdCheck(user_id);
+      if (userIdRows.length < 1)
+        return baseResponse.SIGNIN_EMAIL_WRONG;
+  
+      const selectUserId = userIdRows[0].user_id;
+  
+      // 비밀번호 확인
+      const hashedPassword = await crypto
+          .createHash("sha512")
+          .update(password)
+          .digest("hex");
+  
+      const selectUserPasswordParams = [selectUserId, hashedPassword];
+      const passwordRows = await passwordCheck(
+        selectUserPasswordParams
+      );
+  
+      if (passwordRows[0].password !== hashedPassword) {
+          return baseResponse.SIGNIN_PASSWORD_WRONG;
+      }
+
+      // 계정 상태 확인
+      // const userInfoRows = await accountCheck(user_id);
+  
+      // if (userInfoRows[0].status === "비활성") {
+      //   return errResponse(baseResponse.SIGNIN_INACTIVE_ACCOUNT);
+      // } else if (userInfoRows[0].status === "삭제") {
+      //   return errResponse(baseResponse.SIGNIN_WITHDRAWAL_ACCOUNT);
+      // }
+  
+      //토큰 생성 Service
+      let token = await jwt.sign(
+        {
+          userId: userInfoRows[0].userId,
+          nickname: userInfoRows[0].nickname,
+        }, // 토큰의 내용(payload)
+        secret_config.jwtsecret, // 비밀키
+        {
+          expiresIn: "365d",
+          subject: "userInfo",
+        } // 유효 기간 365일
+      );
+  
+      return response(baseResponse.SUCCESS, {
+        userId: userInfoRows[0].userId,
+        nickname: userInfoRows[0].nickname,
+        jwt: token,
+      });
+    } catch (err) {
+      logger.error(
+        `App - postSignIn Service error\n: ${err.message} \n${JSON.stringify(
+          err
+        )}`
+      );
+      return errResponse(baseResponse.DB_ERROR);
+    }
+  }
