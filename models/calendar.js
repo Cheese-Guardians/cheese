65313fa3e7b6420796875db144bdad8f25661dcd
이하은 <haeun161@naver.com>
2023-05-23 17:22:12 +0900

    관찰일기 띄우기 성공

--- models/calendar.js
+++ models/calendar.js
@@ -9,8 +9,8 @@ async function selectCalendar(pool, userId) {
 }
 //캘린더 조회
 async function getSelectedCalendar(pool, date) {
-    const getHospital_scheduleQuery = `
-      SELECT hospital_name, TIME(booking_time) AS booking_hour
+    const getSelectedCalendarQuery = `
+      SELECT hospital_name
       FROM hospital_schedule
       WHERE user_id = 'handakyeng'
       AND calendar_id = (
@@ -20,9 +20,8 @@ async function getSelectedCalendar(pool, date) {
         AND user_id = 'handakyeng'
       );
     `;
-  
-    const getCheck_listQuery = `
-      SELECT check_content, is_check
+    const getSelectedCalendarCKLQuery = `
+      SELECT check_content
       FROM check_list
       WHERE user_id = 'handakyeng'
       AND calendar_id = (
@@ -32,8 +31,8 @@ async function getSelectedCalendar(pool, date) {
         AND user_id = 'handakyeng'
       );
     `;
-    const getCalendarQuery = `
-      SELECT sleep_time , diary
+    const getSelectedCalendarDiaryQuery = `
+      SELECT diary
       FROM calendar
       WHERE user_id = 'handakyeng'
       AND calendar_id = (
@@ -43,44 +42,21 @@ async function getSelectedCalendar(pool, date) {
         AND user_id = 'handakyeng'
       );
     `;
-    const getSymptomQuery = `
-      SELECT symptom_name, degree
-      FROM symptom
-      WHERE user_id = 'handakyeng'
-      AND calendar_id = (
-        SELECT calendar_id
-        FROM calendar
-        WHERE \`date\` = \'${date}\' 
-        AND user_id = 'handakyeng'
-      );
-    `;
-
     //병원 이름
-    const [rows] = await pool.promise().query(getHospital_scheduleQuery, date);
-    const hospital_scheduler = {
-      hospital_name: "",
-      booking_hour: ""
-    };
-    if (rows.length > 0) {
-      hospital_scheduler.hospital_name = rows[0].hospital_name;
-      hospital_scheduler.booking_hour = rows[0].booking_hour;
-    }
-  
+    const [rows] = await pool.promise().query(getSelectedCalendarQuery, date);
+    const hospitalName = rows.length > 0 ? rows[0].hospital_name : "";
     //체크 사항    
-    const [checkRows] = await pool.promise().query(getCheck_listQuery, date);
-    const check_list  = rows.length > 0 ? checkRows.map(row => row.check_content) : "";
-    //잠잔 시간 //관찰 일기
-    const [diaryRows] = await pool.promise().query(getCalendarQuery, date);
-    const calendarContents  = rows.length > 0 ? diaryRows.map(row => row.diary) : "";
-    //증상
-    const [symptomRows] = await pool.promise().query(getSymptomQuery, date);
-    const symptom  = rows.length > 0 ? symptomRows.map(row => row.diary) : "";
+    const [checkRows] = await pool.promise().query(getSelectedCalendarCKLQuery, date);
+    const checkContents  = rows.length > 0 ? checkRows.map(row => row.check_content) : "";    
+    //관찰 일기
+    const [diaryRows] = await pool.promise().query(getSelectedCalendarDiaryQuery, date);
+    const diaryContents  = rows.length > 0 ? diaryRows.map(row => row.diary) : "";
     //console.log(rows);
     //console.log("date" + date);
-    //console.log("hos: " + hospitalName);
-    // console.log("ck: " + check_list);
-    // console.log("sleepTime: " + calendarContents);
-    return {hospital_scheduler, check_list, calendarContents,symptom};
+    console.log("hos: " + hospitalName);
+    console.log("ck: " + checkContents);
+    console.log("diary: " + diaryContents);
+    return {hospitalName, checkContents, diaryContents};
   }
   
 
