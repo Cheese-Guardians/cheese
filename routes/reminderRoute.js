25ef292159b7c41190c1fca9fd798bbd6113d800
theunghee02 <20211150@sungshin.ac.kr>
2023-05-26 17:59:28 +0900

    setting reminder individually

--- routes/reminderRoute.js
+++ routes/reminderRoute.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const reminder = require('../controllers/reminderController');
+const reminders = require('../controllers/reminderController');
 
+// 화면 조회
 router.get("/", (req, res) => {
     return res.render('reminder/reminder.ejs');
 });
+
+// 문자 보내기
+router.post('/', reminders.sendSMS);
+
+// 복용약 알림 추가
+router.post('/medication', reminders.postMedi);
+
+// 복용약 알림 get
+router.get('/medication', reminders.getMedi);
 
 module.exports = router;
