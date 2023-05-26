b266a0b5acbd0018b6c6a86b6d40f00d5fc0ac40
elive7 <parksub020704@gmail.com>
2023-05-26 16:34:13 +0900

    post시에도 쿼리스트링 그대로 보내기

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -32,38 +32,66 @@ exports.getCalendar = async function (req, res) {
     if (user_id <= 0) {
       return res.send(errResponse(baseResponse.USER_USERIDX_LENGTH));
     }
-    //sconsole.log("date!"+date);
     const calendarResult = await calendarService.retrieveCalendar(user_id);
     const calendarDataResult = await calendarService.retrieveSelectedCalendar(user_id, date);
     
     if (calendarResult.length > 0) {
-      //console.log(calendarResult);
-  
-      //console.log("controller: "+ calendarDataResult);
-      //console.log(calendarResult[calendarResult.length-1].server_name + calendarResult[calendarResult.length-1].extension);
-  
+      
       return res.render('calendar/calendar.ejs', { calendarResult: calendarResult, calendarDataResult: calendarDataResult });
     } else {
       console.log(calendarDataResult);
       return res.render('calendar/calendar.ejs', { calendarResult: null, calendarDataResult: calendarDataResult });
     }
     
-    // return res.send(response(baseResponse.SUCCESS, calendarResult));
   } else {
     return res.redirect('/');
   }
 }
 
 exports.postCalendar = async function (req, res) {
-  if (!req.body) {
-      return req.send(`
-        내용이 없음
-      `);
+  const token = req.cookies.x_auth;
+  if (token) {
+    const decodedToken = jwt.verify(token, secret.jwtsecret); // 토큰 검증, 복호화
+    const user_id = decodedToken.user_id; // user_id를 추출
+    console.log(req.body);
+    let date = req.query.selectedYear + req.query.selectedMonth + req.query.selectedDate;
+    console.log(date);
+    // let date = req.query.selectedYear + req.query.selectedMonth + req.query.selectedDate;
+    // if (!req.query.selectedYear || !req.query.selectedMonth || !req.query.selectedDate) {
+    //   const today = new Date();
+    //   const selectedYear = String(today.getFullYear()).padStart(4, '0');
+    //   const selectedMonth = String(today.getMonth() + 1).padStart(2, '0');
+    //   const selectedDate = String(today.getDate()).padStart(2, '0');
+  
+      
+    //   const existingQueryString = req.query;
+      
+    //   if (Object.keys(existingQueryString).length === 0) {
+    //     const newURL = `${req.protocol}://${req.get('host')}${req.baseUrl}?selectedYear=${selectedYear}&selectedMonth=${selectedMonth}&selectedDate=${selectedDate}`;
+    //     return res.redirect(newURL);
+    //   }
+    // }
+    // validation
+    // if(!user_id) {
+    //   return res.send(errResponse(baseResponse.USER_USERIDX_EMPTY));
+    // } 
+    // if (user_id <= 0) {
+    //   return res.send(errResponse(baseResponse.USER_USERIDX_LENGTH));
+    // }
+    // const calendarResult = await calendarService.retrieveCalendar(user_id);
+    // const calendarDataResult = await calendarService.retrieveSelectedCalendar(user_id, date);
+    
+    // if (calendarResult.length > 0) {
+      
+    //   return res.render('calendar/calendar.ejs', { calendarResult: calendarResult, calendarDataResult: calendarDataResult });
+    // } else {
+    //   console.log(calendarDataResult);
+    //   return res.render('calendar/calendar.ejs', { calendarResult: null, calendarDataResult: calendarDataResult });
+    // }
+    
+  } else {
+    return res.send('calendar req error');
   }
-
-  console.log(req.body);
-  //return res.redirect('/calendar');
-  res.send(req.body);
 };
 
 exports.postFile = async function (req, res) {
