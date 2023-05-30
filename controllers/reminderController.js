9e9a35e449d6aa3c6c4ca4f0b765699fc99c5451
Minju Park <80474535+parkminju20211126@users.noreply.github.com>
2023-05-31 01:18:23 +0900

    connect reminder.ejs

--- controllers/reminderController.js
+++ controllers/reminderController.js
@@ -1,12 +1,20 @@
 const reminderService = require('../services/reminderService');
+const jwt = require('jsonwebtoken');
+const secret = require('../config/secret');
+const path = require('path');
 exports.postMedi = async function (req, res) {
+    const token = req.cookies.x_auth;
+    const decodedToken = jwt.verify(token, secret.jwtsecret); // 토큰 검증, 복호화
+    const user_id = decodedToken.user_id; // user_id를 추출
+
     const {
-        user_id,
         medi_reminder_time
     } = req.body;
+    console.log(req.body);
     const MediResponse = await reminderService.createMediReminder(
         user_id,
         medi_reminder_time
     );
+    
    return res.send(MediResponse);
 };
--- routes/reminderRoute.js
+++ routes/reminderRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const reminders = require('../controllers/reminderController');
+const reminderController = require('../controllers/reminderController');
 
 // 화면 조회
 router.get("/", (req, res) => {
@@ -11,7 +11,7 @@ router.get("/", (req, res) => {
 // router.post('/', reminders.sendSMS);
 
 // 복용약 알림 추가
- router.post('/medication', reminders.postMedi);
+ router.post('/medication', reminderController.postMedi);
 
 // // 복용약 알림 get
  //router.get('/medication', reminders.getMedi);
