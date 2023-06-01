bf083e075f737a2420d4ef106ec7b33da5eda679
theunghee02 <20211150@sungshin.ac.kr>
2023-06-01 12:30:36 +0900

    [Fix] GET reminder

--- routes/reminderRoute.js
+++ routes/reminderRoute.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const reminderController = require('../controllers/reminderController');
 
 // 화면 조회
-router.get("/", (req, res) => {
+router.get("/", reminderController.getMedi, (req, res) => {
     return res.render('reminder/reminder.ejs');
 });
 
 // SMS 메시지 보내기를 처리하는 라우트 핸들러
 // 문자인증(SENS를 통한) 전송 API
-app.post('/send', reminders.sendSMS);
+app.post('/send', reminderController.sendSMS);
 
 // 문자 보내기
 // router.post('/', reminders.sendSMS);
@@ -17,8 +17,8 @@ app.post('/send', reminders.sendSMS);
 // 복용약 알림 추가
  router.post('/medication', reminderController.postMedi);
 
-// // 복용약 알림 get
- //router.get('/medication', reminders.getMedi);
+// 복용약 알림 get
+//  router.get('/medication', reminderController.getMedi);
 
 // 병원 일정 알림 get
 // router.get('/hospital', reminders.getHospital);
