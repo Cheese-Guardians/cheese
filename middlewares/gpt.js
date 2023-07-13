4400487b45c9e9c74374482047d6041d485342fd
theunghee02 <20211150@sungshin.ac.kr>
2023-07-13 22:31:39 +0900

    start chatGPT api

---
+++ middlewares/gpt.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    organization: "org-9eK5ps4TUckwvyKZOubms7M6",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.engines.list();
