28ad8449ec61eb2860a200d9f0cd2686d7f6eaad
theunghee02 <20211150@sungshin.ac.kr>
2023-07-14 20:55:29 +0900

    feat exportController

--- controllers/exportController.js
+++ controllers/exportController.js
@@ -5,11 +5,41 @@ const axios = require('axios');
 
 exports.postSummary = async function (req, res) {
     try {
-        const user_id = 'test';
-
+        const {
+            user_id,
+            date1,
+            date2
+        } = req.body;
+        const diaryResponse = await exportService.retrieveSelectedDiary(user_id, date1, date2);
+        console.log(summarizeDiary(diaryResponse));
         
     } catch (error) {
         console.error('Error:', error);
         throw new Error('Failed to summarize diary.');
     }
 }
+
+// GPT API
+async function summarizeDiary(diaryResponse) {
+    try {
+      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: 'You are a diary summarizer.' },
+          { role: 'user', content: diaryResponse },
+        ],
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+      });
+  
+      // 요약된 내용은 response.data.choices[0].message.content에서 확인할 수 있습니다.
+      const summary = response.data.choices[0].message.content;
+      return summary;
+    } catch (error) {
+      console.error('Error:', error);
+      throw new Error('Failed to summarize diary.');
+    }
+  }
