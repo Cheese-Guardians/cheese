ca97a93f74e6c7c7f852d1c929efac3618ced57b
handagyeong <80438961+handagyeong@users.noreply.github.com>
2023-07-14 22:05:23 +0900

    feat: gpt일기요약 근데 영어

--- controllers/exportController.js
+++ controllers/exportController.js
@@ -5,23 +5,19 @@ const axios = require('axios');
 
 exports.postSummary = async function (req, res) {
     try {
-        const {
-            user_id,
-            date1,
-            date2
-        } = req.body;
-        const diaryResponse = await exportService.retrieveSelectedDiary(user_id, date1, date2);
-        console.log("hihihih", diaryResponse,"hihihihi")
-        console.log(summarizeDiary(diaryResponse));
-        
+      const { user_id, date1, date2 } = req.body;
+      const diaryResponse = await exportService.retrieveSelectedDiary(user_id, date1, date2);
+      const diaryText = diaryResponse.calendar.diary.join(' ');
+      const summary = await summarizeDiary(diaryText);
+      res.json({ summary });
     } catch (error) {
-        console.error('Error:', error);
-        throw new Error('Failed to summarize diary.');
+      console.error('Error:', error);
+      throw new Error('Failed to summarize diary.');
     }
-}
-
-// GPT API
-async function summarizeDiary(diaryResponse) {
+  }
+  
+  // GPT API
+  async function summarizeDiary(diaryResponse) {
     try {
       const response = await axios.post('https://api.openai.com/v1/chat/completions', {
         model: 'gpt-3.5-turbo',
@@ -38,9 +34,11 @@ async function summarizeDiary(diaryResponse) {
   
       // 요약된 내용은 response.data.choices[0].message.content에서 확인할 수 있습니다.
       const summary = response.data.choices[0].message.content;
+      console.log("\n\n결과: ",summary);
       return summary;
     } catch (error) {
       console.error('Error:', error);
       throw new Error('Failed to summarize diary.');
     }
   }
+  
