c2c0d4a65621fe28d4192170105e5a78c8db1b9f
haeun161 <haeun161@naver.com>
2023-09-29 22:59:41 +0900

    fix: 전체통계와 증상별 통계 그래프 한 python파일에 합체

--- public/statistic.py
+++ public/statistic.py
@@ -3,8 +3,37 @@ import matplotlib.pyplot as plt
 
 # 한글 폰트 설정
 plt.rcParams['font.family'] = 'Malgun Gothic'
-plt.rcParams['axes.unicode_minus'] = False
+plt.rcParams['axes.unicoㅁde_minus'] = False
 
+#증상별 통계(x축:날짜, y축:척도)
+# CSV 파일 경로 설정
+file_path = 'csv/eachSymptom.csv'
+
+# CSV 파일 읽기
+dataset = pd.read_csv(file_path, delimiter=',')
+dataset['date'] = pd.to_datetime(dataset['date'])  # 'date' 컬럼을 날짜로 변환
+#print(dataset)
+dataset = dataset.sort_values(by=['symptom_name', 'date'])  # 'symptom_name'과 'date'로 정렬
+
+# 각 증상별로 그래프 생성 및 저장
+symptoms = dataset['symptom_name'].unique()
+
+for symptom in symptoms:
+    plt.figure(figsize=(5, 5))
+    symptom_data = dataset[dataset['symptom_name'] == symptom]
+    plt.plot(symptom_data['date'], symptom_data['degree'], label=symptom)
+    plt.xlabel('날짜')
+    plt.ylabel('척도')
+    plt.title(symptom )
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.tight_layout()
+
+    #plt.show()
+    plt.savefig(f'public/output/{symptom}.png')
+    plt.close()
+
+#전체통계
 # 데이터 파일 로드
 entire_symptom_df = pd.read_csv('csv/entireSymptom.csv')
 last_entire_symptom_df = pd.read_csv('csv/lastEntireSymptom.csv')
@@ -54,7 +83,6 @@ plt.xticks(rotation=0)  # x축 레이블 회전 방지
 
 # x축 레이블을 증상 이름으로 설정
 ax.set_xticklabels(this_month.index.get_level_values('symptom_name'))
-
 
 #plt.show()
 plt.savefig(f'public/output/all.png')
